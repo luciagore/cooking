85f19835a42b3b9d9c835d0f05a82bec45327191
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2018-09-21 14:29:33 +0100

    Progressing towards a good test setup.

--- test/unitTests/api-actions.test.js
+++ test/unitTests/api-actions.test.js
@@ -1,18 +1,21 @@
 const actions = require('../../lib/api-actions');
 
-var collection = { "recipeName" : "pancakes", "cookingTime" : "10", "ingredients" : "eggs milk", "method" : "bake" }
-var recipe = { "recipeName" : "PANCAKES", "cookingTime" : "10", "ingredients" : "EGGS MILK", "method" : "BAKE" }
-
-
-mock = jest.fn()
-mock.mockReturnValue(collection)
+var recipe = { "recipeName" : "pancakes", "cookingTime" : "10", "ingredients" : "eggs milk", "method" : "bake" }
 
 test('returns a document', () => {
-    // const find = jest.fn()
-    // find.mockReturnValue(collection);
-    // console.log(find('blaaaaaah'));
-    expect(mock(actions.getDocument(collection, "j:5ba0fc6684357e0295ff472d", 'pancakes', collection))).toEqual([recipe])
-    // // expect(find("getDocument()")).toEqual(recipe)
-    expect(mock).toHaveBeenCalled();
-    // expect(getDocument(collection, userID, query, next/callback)).toBe([recipe]);
-  });
+    const mockCollection = {
+        find: function(userID, something, next) {
+            const mockedResponse = [{ 
+                "recipeName" : "pancakes", 
+                "cookingTime" : "30", 
+                "ingredients" : "eggs milk", 
+                "method" : "bake" }];
+            next(mockedResponse)
+        }
+    }
+    actions.getDocument(mockCollection, 'one', 'pancakes', function(data) {
+            console.log(data);
+          });
+    
+    expect(actions.getDocument(mockCollection, "j:5ba0fc6684357e0295ff472d", 'pancakes', 'WHAT DO I PASS HERE?')).toEqual([recipe])
+})
