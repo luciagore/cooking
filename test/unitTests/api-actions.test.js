5e368aaf781786230435303bc320c2c0ff3ff6bb
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2018-09-21 17:52:41 +0100

    a bit of description for the parts

--- test/unitTests/api-actions.test.js
+++ test/unitTests/api-actions.test.js
@@ -3,6 +3,7 @@ const actions = require('../../lib/api-actions');
 var recipe = { "recipeName" : "pancakes", "cookingTime" : "10", "ingredients" : "eggs milk", "method" : "bake" }
 
 test('returns a document', (done) => {
+    //setup
     const mockCollection = {
         find: function(userID, something, next) {
             const mockedResponse = [{ 
@@ -13,7 +14,9 @@ test('returns a document', (done) => {
             next(mockedResponse)
         }
     }
+        // execution of our method
         actions.getDocument(mockCollection, 'one', 'eggs', (recipes) => {
+        // actual test
         expect(recipes).toEqual([recipe])
         done();
     });
