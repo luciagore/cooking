9ed01673a91150d06392f4626d095bf4d3d3cb9f
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2018-09-21 16:57:33 +0100

    First passing unit test with mock on collectiobn.find

--- test/unitTests/api-actions.test.js
+++ test/unitTests/api-actions.test.js
@@ -2,20 +2,20 @@ const actions = require('../../lib/api-actions');
 
 var recipe = { "recipeName" : "pancakes", "cookingTime" : "10", "ingredients" : "eggs milk", "method" : "bake" }
 
-test('returns a document', () => {
+test('returns a document', (done) => {
     const mockCollection = {
         find: function(userID, something, next) {
             const mockedResponse = [{ 
                 "recipeName" : "pancakes", 
-                "cookingTime" : "30", 
+                "cookingTime" : "10", 
                 "ingredients" : "eggs milk", 
                 "method" : "bake" }];
             next(mockedResponse)
         }
     }
-    actions.getDocument(mockCollection, 'one', 'pancakes', function(data) {
-            console.log(data);
-          });
+        actions.getDocument(mockCollection, 'one', 'eggs', (recipes) => {
+        expect(recipes).toEqual([recipe])
+        done();
+    });
     
-    expect(actions.getDocument(mockCollection, "j:5ba0fc6684357e0295ff472d", 'pancakes', 'WHAT DO I PASS HERE?')).toEqual([recipe])
-})
+});
