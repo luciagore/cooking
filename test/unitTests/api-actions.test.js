e255069f29913dc9d9f1b1b283e65b156924fd62
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2018-09-22 18:29:29 +0100

    Using Test Suites

--- test/unitTests/api-actions.test.js
+++ test/unitTests/api-actions.test.js
@@ -1,60 +1,66 @@
 const actions = require('../../lib/api-actions');
 
-var recipe = { "recipeName" : "pancakes", "cookingTime" : "10", "ingredients" : "eggs milk", "method" : "bake" }
 
-test('returns a document', (done) => {
-    //setup
-    const mockCollection = {
-        find: function(userID, something, next) {
-            const mockedResponse = [{ 
-                "recipeName" : "pancakes", 
-                "cookingTime" : "10", 
-                "ingredients" : "eggs milk", 
-                "method" : "bake" }];
-            next(mockedResponse)
+describe('getting documents', () => {
+    var recipe = { "recipeName": "pancakes", "cookingTime": "10", "ingredients": "eggs milk", "method": "bake" }
+    test('returns a document', (done) => {
+        //setup
+        const mockCollection = {
+            find: function (userID, something, next) {
+                const mockedResponse = [{
+                    "recipeName": "pancakes",
+                    "cookingTime": "10",
+                    "ingredients": "eggs milk",
+                    "method": "bake"
+                }];
+                next(mockedResponse)
+            }
         }
-    }
         // execution of our method
         actions.getDocument(mockCollection, 'one', 'eggs', (recipes) => {
-        // actual test
-        expect(recipes).toEqual([recipe])
-        done();
+            // actual test
+            expect(recipes).toEqual([recipe])
+            done();
+        });
     });
-});
-
-test('returns a collection of documents', (done) => {
-    // setup
-    let docs = [{
-        "cookingTime": "10",
-        "ingredients": "eggs milk",
-        "method": "bake",
-        "recipeName": "pancakes"}, 
-        {"cookingTime": "30",
-        "ingredients": "celery onios potatoes",
-        "method": "cook",
-        "recipeName": "soup"
-    }]
-    const mockedCollection = {
-        find: function(userID, someObject, next){
-            const mockedResponse = [
-                {
-                "recipeName" : "pancakes", 
-                "cookingTime" : "10", 
-                "ingredients" : "eggs milk", 
-                "method" : "bake" },
-                {
-                "recipeName" : "soup", 
-                "cookingTime" : "30", 
-                "ingredients" : "celery onios potatoes", 
-                "method" : "cook" }];
-            next(mockedResponse)
+    
+    test('returns a collection of documents', (done) => {
+        // setup
+        let docs = [{
+            "cookingTime": "10",
+            "ingredients": "eggs milk",
+            "method": "bake",
+            "recipeName": "pancakes"
+        },
+        {
+            "cookingTime": "30",
+            "ingredients": "celery onios potatoes",
+            "method": "cook",
+            "recipeName": "soup"
+        }]
+        const mockedCollection = {
+            find: function (userID, someObject, next) {
+                const mockedResponse = [
+                    {
+                        "recipeName": "pancakes",
+                        "cookingTime": "10",
+                        "ingredients": "eggs milk",
+                        "method": "bake"
+                    },
+                    {
+                        "recipeName": "soup",
+                        "cookingTime": "30",
+                        "ingredients": "celery onios potatoes",
+                        "method": "cook"
+                    }];
+                next(mockedResponse)
             }
         }
         // exercise getDocuments method
-        actions.getDocuments(mockedCollection, 'two', (docs) =>{
+        actions.getDocuments(mockedCollection, 'two', (docs) => {
             // actual test
-        expect(docs).toEqual(docs)
-        done();
+            expect(docs).toEqual(docs)
+            done();
         })
     })
-    
+});
