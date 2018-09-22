30b147f63194d2577275417b49872bafa924473b
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2018-09-22 14:50:35 +0100

    Passing test for getDocuments

--- test/unitTests/api-actions.test.js
+++ test/unitTests/api-actions.test.js
@@ -20,5 +20,41 @@ test('returns a document', (done) => {
         expect(recipes).toEqual([recipe])
         done();
     });
-    
 });
+
+test('returns a collection of documents', (done) => {
+    // setup
+    let docs = [{
+        "cookingTime": "10",
+        "ingredients": "eggs milk",
+        "method": "bake",
+        "recipeName": "pancakes"}, 
+        {"cookingTime": "30",
+        "ingredients": "celery onios potatoes",
+        "method": "cook",
+        "recipeName": "soup"
+    }]
+    const mockedCollection = {
+        find: function(userID, someObject, next){
+            const mockedResponse = [
+                {
+                "recipeName" : "pancakes", 
+                "cookingTime" : "10", 
+                "ingredients" : "eggs milk", 
+                "method" : "bake" },
+                {
+                "recipeName" : "soup", 
+                "cookingTime" : "30", 
+                "ingredients" : "celery onios potatoes", 
+                "method" : "cook" }];
+            next(mockedResponse)
+            }
+        }
+        // exercise getDocuments method
+        actions.getDocuments(mockedCollection, 'two', (docs) =>{
+            // actual test
+        expect(docs).toEqual(docs)
+        done();
+        })
+    })
+    
