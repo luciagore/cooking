a201a5b85b21620422e4c72b96472242a4a299ed
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2018-09-23 01:07:52 +0100

    Green test for 'getRecipesByTItle'. Correct params to the .find callback

--- test/unitTests/api-actions.test.js
+++ test/unitTests/api-actions.test.js
@@ -1,7 +1,7 @@
 const actions = require('../../lib/api-actions');
 
 
-describe('getting recipes', () => {
+describe('Getting recipes', () => {
     var recipe = { 
         "recipeName": "pancakes",
         "cookingTime": "10",
@@ -18,11 +18,32 @@ describe('getting recipes', () => {
                     "ingredients": "eggs milk",
                     "method": "bake"
                 }];
-                next(mockedResponse)
+                next(null, mockedResponse)
             }
         }
         // execution of our method
         actions.getRecipeByIngredients(mockCollection, 'one', 'eggs milk', (recipes) => {
+            // actual test
+            expect(recipes).toEqual([recipe])
+            done();
+        });
+
+    });
+    test('returns a recipe by title', (done) => {
+        //setup
+        const mockCollection = {
+            find: function (userID, something, next) {
+                const mockedResponse = [{
+                    "recipeName": "pancakes",
+                    "cookingTime": "10",
+                    "ingredients": "eggs milk",
+                    "method": "bake"
+                }];
+                next(null, mockedResponse)
+            }
+        }
+        // execution of our method
+        actions.getRecipeByTitle(mockCollection, 'one', 'pancakes', (recipes) => {
             // actual test
             expect(recipes).toEqual([recipe])
             done();
@@ -58,7 +79,7 @@ describe('getting recipes', () => {
                         "ingredients": "celery onios potatoes",
                         "method": "cook"
                     }];
-                next(mockedResponse)
+                next(null, mockedResponse)
             }
         }
         // exercise getRecipes method
