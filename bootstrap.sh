3d8a4dddebb20a4640106e8babc861ea93e40f2d
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2018-10-02 16:46:07 +0100

    Config files for Front End and Database

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+
 sudo apt-get update -y
 sudo apt-get install -y git
 sudo apt-get install -y curl
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
-sudo apt-get install -y mongodb
 
-sudo mkdir -p /ecsdroot/app
-cd /ecsdroot/app
+sudo mkdir -p /app
+cd /app
 sudo git clone https://github.com/luciagore/cooking.git
 cd cooking
 
---
+++ db_boothstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y mongodb
+
+sudo mkdir -p /data/db
+sudo chmod -R go+w /data/db
+mongod
