12ba1d77431128be436d81d5cf09501c4a8d5e55
Matt Vickers <mattvickers@ECS0787.local>
2018-10-08 10:11:56 +0100

    corrects db bootstrap script

--- db_boothstrap.sh
+++ db_boothstrap.sh
@@ -4,7 +4,3 @@ sudo apt-get update -y
 sudo apt-get install -y mongodb
 sudo sed -i 's|bind_ip = 127.0.0.1|bind_ip = 0.0.0.0|g' /etc/mongodb.conf
 sudo service mongodb restart
-
-sudo mkdir -p /data/db
-sudo chmod -R go+w /data/db
-sudo service mongodb start
